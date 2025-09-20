b01215b2fd48d84a2b1a12cdbc1ad43647f7ac2b
natemac <nateofs@gmail.com>
2025-09-20 08:49:43 -0500

    home page update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Game from './components/game/Game'
 import Builder from './components/builder/Builder'
